a7aeef70c876fd03bf19bdbd3e74bf461917fb17
Janxyxy <jankroupa2@gmail.com>
2024-05-25 00:15:36 +0200

    Update

--- hackiton/hackiton/urls.py
+++ hackiton/hackiton/urls.py
@@ -20,4 +20,6 @@ from django.urls import include, path
 urlpatterns = [
     path('', include('udesky.urls')),
     path('admin/', admin.site.urls),
+    path("__reload__/", include("django_browser_reload.urls")),
+
 ]
