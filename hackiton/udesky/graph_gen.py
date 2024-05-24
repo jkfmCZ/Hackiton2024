8f4c4064a19aa33b1edf2d4ce06e4854d9290089
jkfmCZ <88970329+jkfmCZ@users.noreply.github.com>
2024-05-25 01:57:54 +0200

    update

--- hackiton/udesky/graph_gen.py
+++ hackiton/udesky/graph_gen.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import json
 
 
 dfkraje = pd.read_csv("udesky/duchodci-v-cr-krajich-okresech.csv")
@@ -8,4 +9,30 @@ duchodci = dfkraje[
         (dfkraje["druh_duchodu"] == "Starobní důchod S") & 
         (dfkraje["pohlavi"] == "Celkem") & 
         (dfkraje["referencni_oblast"] == "Česká republika")]
+
+
+
+file = "hackiton/udesky/data/soudy_decin.jsonld"
+with open(file, 'r', encoding='utf-8') as file:
+    data = json.load(file)
+
+
+df = pd.json_normalize(data['informace'])
+# print(df)
+df['vyvěšení.datum_a_čas'] = pd.to_datetime(df['vyvěšení.datum_a_čas'])
+
+
+df['rok_vyvěšení'] = df['vyvěšení.datum_a_čas'].dt.year
+df["count"] = 1
+df_count_per_year = df.groupby('rok_vyvěšení').size().reset_index(name='count')
+
+
+df_count_per_year.reset_index(drop=True, inplace=True)
+
+# print(df_count_per_year)
+data = {
+    'název_obce': ["Děčín"],
+    '2023': [df_count_per_year["count"].loc[0]],
+    '2024': [df_count_per_year["count"].loc[1]]}
+df2 = pd.DataFrame(data)
 
